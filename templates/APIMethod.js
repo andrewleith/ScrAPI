51336a1b915b8892b419bf352f096744ce6c0181
Andrew <andrew.leith@gmail.com>
2014-07-15 23:10:56 -0400

    Removed unnecessary code.

--- templates/APIMethod.js
+++ templates/APIMethod.js
@@ -2,12 +2,6 @@
 app.get('/%%URI%%', function(req, res) {
 
     var options = %%OPTIONS%%;
-    // how the hell do you check if req.query exists?
-    var hasParams = false;
-    for (var keys in req.query) {
-        hasParams = true;
-        break;
-    }
    
     // map method params to target site's query string params [TODO: support more ways of mapping params (post, routes, whatever)]
     if (options.inputMappings) {
